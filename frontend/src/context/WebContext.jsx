d4e1a6efd4ff80e0c33503bd525e1a023e295bb9
SamTheInfernoTest <saurabhinferno8@gmail.com>
2024-11-21 19:22:32 +0530

    Update WebContext.jsx

--- frontend/src/context/WebContext.jsx
+++ frontend/src/context/WebContext.jsx
@@ -12,7 +12,8 @@ export function WebContextProvider({ children }) {
     const [themeMenu, setThemeMenu] = useState('hidden');
     const [mode, setMode] = useState(localTheme || 'system');
 
-    const baseApiUrl = 'http://127.0.0.1:8000'
+    // const baseApiUrl = 'http://127.0.0.1:8000'
+    const baseApiUrl = 'https://tmsbackend-lui2nqzdb-samtheinfernotests-projects.vercel.app'
 
     // For reference https://coolors.co/palette/03045e-023e8a-0077b6-0096c7-00b4d8-48cae4-90e0ef-ade8f4-caf0f8
     
